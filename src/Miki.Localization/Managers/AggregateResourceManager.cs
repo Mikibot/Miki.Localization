faf04053251c54a08475867ff4446340c7c2d062
Gerard Smit <gsmit1996@gmail.com>
2020-06-07 20:23:16 +0200

    Added AggregateResourceManager
    The aggregate ResourceManager will loop through multiple IResourceManagers until it finds a string.

---
+++ src/Miki.Localization/Managers/AggregateResourceManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Miki.Functional;
+
+namespace Miki.Localization
+{
+    public class AggregateResourceManager : IResourceManager
+    {
+        private readonly IList<IResourceManager> resourceManagers;
+
+        public AggregateResourceManager()
+        {
+            resourceManagers = new List<IResourceManager>();
+        }
+
+        public AggregateResourceManager(IEnumerable<IResourceManager> resourceManagers)
+        {
+            this.resourceManagers = new List<IResourceManager>(resourceManagers);
+        }
+
+        public AggregateResourceManager(params IResourceManager[] resourceManagers)
+            : this(resourceManagers.AsEnumerable())
+        {
+        }
+
+        public void Add(IResourceManager resourceManager)
+        {
+            resourceManagers.Add(resourceManager);
+        }
+
+        /// <inheritdoc />
+        public Optional<string> GetString(Required<string> key)
+        {
+            if (resourceManagers.Count == 1)
+            {
+                return resourceManagers[0].GetString(key);
+            } 
+            
+            return resourceManagers
+                .Select(resourceManager => resourceManager.GetString(key))
+                .First(result => result.HasValue);
+        }
+    }
+}
